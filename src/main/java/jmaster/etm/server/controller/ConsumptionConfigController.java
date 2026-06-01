2650bd393621f4bb91b5cad8d744909343519c84
timur.kozlov <timur.kozlov@gmail.com>
2026-06-01 15:38:25 +0300

    layout

--- src/main/java/jmaster/etm/server/controller/ConsumptionConfigController.java
+++ src/main/java/jmaster/etm/server/controller/ConsumptionConfigController.java
@@ -1,30 +1,20 @@
 package jmaster.etm.server.controller;
 
-import com.turkraft.springfilter.converter.FilterSpecification;
 import jmaster.core.controller.AbstractController;
-import jmaster.etm.server.model.ConsumptionDataset;
-import jmaster.etm.server.model.ConsumptionSnapshot;
 import jmaster.etm.server.service.ConsumptionRegisterService;
 import jmaster.etm.server.service.ConsumptionReportService;
 import jmaster.etm.server.service.FetchConfig;
 import jmaster.etm.server.service.LastError;
 import jmaster.system.prefs.PrefsService;
-import org.hibernate.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Collection;
 
 @Controller
-public class ConsumptionController extends AbstractController {
+public class ConsumptionConfigController extends AbstractController {
 	
 	@Autowired
     ConsumptionReportService consumptionReportService;
