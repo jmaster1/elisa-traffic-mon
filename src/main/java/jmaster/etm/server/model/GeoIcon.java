01ce3f55ef18cdb926b1f8440a5b372d10c024d7
timur.kozlov <timur.kozlov@gmail.com>
2026-06-03 15:27:07 +0300

    Go rip

--- src/main/java/jmaster/etm/server/model/GeoIcon.java
+++
@@ -1,8 +0,0 @@
-package jmaster.etm.server.model;
-
-public interface GeoIcon {
-    String DEVICE = "phone";
-    String WORKSITE = "building";
-    String USER_WORKSITE = "person-workspace";
-    String CLIENT_SESSION = "play-circle";
-}
